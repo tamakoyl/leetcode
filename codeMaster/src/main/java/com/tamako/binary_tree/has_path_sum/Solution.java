e0d202b4138dee6365eacc11e94faf6c1c137732
tamako <884920265@qq.com>
2024-01-21 09:13:25 +0800

    fix:修改一下112路径总和

--- codeMaster/src/main/java/com/tamako/binary_tree/has_path_sum/Solution.java
+++ codeMaster/src/main/java/com/tamako/binary_tree/has_path_sum/Solution.java
@@ -11,25 +11,45 @@ public class Solution {
     private int sum = 0;
 
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        //递归出口
         if (root == null) return false;
+        return traversal(root,targetSum - root.val);
+    }
+
+
+    public boolean concise(TreeNode root,int cnt){
+        if(root == null)return false;
+        //到达叶子结点
+        if (root.left == null && root.right == null && cnt == root.val) return true;
+        return hasPathSum(root.left,cnt-root.val) || hasPathSum(root.right,cnt-root.val);
+    }
+    public boolean traversal(TreeNode root,int cnt){
+        //递归出口
 
         //到达叶子结点
-        if (root.left == null && root.right == null) {
-            sum += root.val;
-            if (sum == targetSum) {
-                return true;
-            } else {
-                sum -= root.val;
-                return false;
-            }
+        if (root.left == null && root.right == null && cnt == 0) return true;
+
+        //到达叶子结点 但是值不相等的时候
+        if (root.left == null && root.right == null) return false;
+
+        //左边
+        if(root.left !=null){
+            cnt -= root.left.val;
+            if(traversal(root.left,cnt))return true;
+            //回溯
+            cnt += root.left.val;
         }
-        sum += root.val;
-        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+        //右边
+        if(root.right != null){
+            cnt -= root.right.val;
+            if(traversal(root.right,cnt))return true;
+            //回溯
+            cnt += root.right.val;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         TreeNode root = TreeNode.buildTreeByArray(new Integer[]{1,-2,-3,1,3,-2,null,-1});
-        new Solution().hasPathSum(root, 22);
+        new Solution().hasPathSum(root, 3);
     }
 }

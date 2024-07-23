f57a8b9d0795050e10c16315df5bddd4fc2ddd37
tamako <884920265@qq.com>
2024-07-23 08:52:59 +0800

    add:完成41-缺失的第一个正数

---
+++ codeMaster/src/main/java/com/hot100/array/FirstMissingPositive_41.java
@@ -0,0 +1,29 @@
+package com.hot100.array;
+
+public class FirstMissingPositive_41 {
+
+    public static void main(String[] args) {
+        firstMissingPositive(new int[] { 3, 4, -1, 1 });
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && i + 1 != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
